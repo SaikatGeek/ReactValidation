244fb7e48360c616a13417cc7264719714db6b9a
SaikatGeek <saikat.geek@gmail.com>
2021-03-03 14:11:47 +0600

    api axios update

--- Validation.js
+++ Validation.js
@@ -51,3 +51,70 @@ className={ errorHas.password ? errorMessage.isInvalid : validMessage.passwordSt
 >
   Login
 </button>   
+
+
+### Axios.js
+
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-type": "application/json"
+  }
+});
+
+
+#### Api.js
+import http from "./Axios";
+
+const getAll = () => {
+  return http.get("/tutorials");
+};
+
+const get = id => {
+  return http.get(`/tutorials/${id}`);
+};
+
+const login = data => {
+  return http.post("/login", data);
+};
+
+const update = (id, data) => {
+  return http.put(`/tutorials/${id}`, data);
+};
+
+const remove = id => {
+  return http.delete(`/tutorials/${id}`);
+};
+
+const removeAll = () => {
+  return http.delete(`/tutorials`);
+};
+
+const findByTitle = title => {
+  return http.get(`/tutorials?title=${title}`);
+};
+
+export default {
+  getAll,
+  get,
+  login,
+  update,
+  remove,
+  removeAll,
+  findByTitle
+};
+
+
+### Widget/file.js
+
+const submitForm = () =>{
+    Api.login(values)
+      .then(response => {        
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
